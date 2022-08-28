feee4f7d80d678384bf0d65bff05bc73fb3dd903
Eliseo Flores <eliseo_flores530@yahoo.com>
2022-08-27 18:32:59 -0700

    added proxy server and front end ui to create orders and retrieve them

--- src/App.js
+++ src/App.js
@@ -1,25 +1,123 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+
+
+function orderCoffee(size, name, type, cb) {
+  fetch(`/graphql`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      query: `mutation {
+        orderCoffee(size: ${size}, name: "${name}", type: ${type}) {
+          id
+          name
+          type
+          size
+        }
+      }`,
+    }),
+  })
+    .then(res => res.json())
+    .then(res => cb(res.data))
+    .catch(console.error)
+}
+
+function getOrders(cb) {
+  fetch(`/graphql`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      query: `query {
+        orders {
+          id
+          name
+          type
+          size
+        }
+      }`,
+    }),
+  })
+    .then((res) => res.json())
+    .then((res) => cb(res.data.orders))
+    .catch(console.error);
+}
 
 function App() {
+  const [myOrders, setMyOrders] = useState([])
+  const [order, setOrder] = useState({
+    size: "SMALL",
+    type: "FLAT_WHITE",
+    name: "",
+  })
+
+  useEffect(() => {
+    getOrders((data) => setMyOrders(data));
+  }, []);
+
+  function onSubmitOrderForm(e) {
+    e.preventDefault()
+
+    // Let's create this API call shortly
+    orderCoffee(order.size, order.name, order.type, ({ orderCoffee }) => {
+      setMyOrders([...myOrders, orderCoffee])
+    })
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form onSubmit={onSubmitOrderForm}>
+          <label>
+            Size:
+            <select
+              onChange={({ target }) =>
+                setOrder({ ...order, size: target.value })
+              }
+            >
+              <option value="SMALL" defaultValue>
+                Small
+              </option>
+              <option value="REGULAR">Regular</option>
+            </select>
+          </label>
+          <label>
+            Name:{" "}
+            <input
+              type="text"
+              onChange={({ target }) =>
+                setOrder({ ...order, name: target.value })
+              }
+            />
+          </label>
+          <label>
+            <select
+              onChange={({ target }) =>
+                setOrder({ ...order, type: target.value })
+              }
+            >
+              Type:
+              <option value="FLAT_WHITE" defaultValue>
+                Flat White
+              </option>
+              <option value="ESPRESSO">Espresso</option>
+            </select>
+          </label>
+          <input type="submit" value="Order Coffee" />
+        </form>
+        <ul>
+          {myOrders.map(order => (
+            <li id={order.id}>
+              {order.name} - {order.size.toLowerCase()}{" "}
+              {order.type.split("_").join(" ").toLowerCase()}
+            </li>
+          ))}
+        </ul>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
